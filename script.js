f34d435d56df3060887c9a3297ab4b0e78175646
FlameWing2 <aalbor08@gmail.com>
2022-09-28 18:26:13 -0300

    Add playRound function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const RPS = ['Rock', 'Paper', 'Scissors'];
+const RPS = ['rock', 'paper', 'scissors'];
 
 const choice = () => RPS[Math.floor(Math.random()*3)]
 
 const getComputerChoice = () => choice(); 
 
-console.log(getComputerChoice())
-
+const playRound = ( playerSelection, computerSelection) => {
+    if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" &&     computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper" ) {
+        return console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+    } else if (playerSelection === "scissor" && computerSelection === "rock" || playerSelection === "rock" &&     computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" ) {
+        return console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+    } else console.log('Its a tie!')
+}
 
